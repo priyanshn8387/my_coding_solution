943c87bd1c4371e4e0360ff87f5f6d9a09a98998
Priyansh Nagar <50418088+priyanshn8387@users.noreply.github.com>
2024-12-18 16:00:42 +0530

    Time: 10 ms (92.26%), Space: 44.6 MB (55.51%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,32 +1,36 @@
 class Solution {
     public int maxVowels(String s, int k) {
 
-        Queue<Character> qu = new LinkedList<>();
+       // Queue<Character> qu = new LinkedList<>(); // not needed queue
         int maxVowelCount = Integer.MIN_VALUE;
+       int  vowelCount = 0;
 
         for(int i=0;i<k;i++){
             char ch = s.charAt(i);
             if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
-                qu.offer(ch);
+               // qu.offer(ch);
+               vowelCount++;
             }
         }
 
-        maxVowelCount = Math.max(maxVowelCount,qu.size());
+         maxVowelCount = Math.max(maxVowelCount,vowelCount);
 
         for(int i=k;i<s.length();i++){
             char incoming = s.charAt(i);
             if(incoming == 'a' || incoming == 'e' || incoming == 'i' || incoming == 'o' ||  incoming == 'u'){
-                qu.offer(incoming);
+                //qu.offer(incoming);
+                vowelCount++;
             }
 
             char outgoing = s.charAt(i-k);
             if(outgoing == 'a' || outgoing == 'e' || outgoing == 'i' || outgoing == 'o' ||  outgoing == 'u'){
-                if(!qu.isEmpty()){
-                    qu.poll();
-                } 
+                // if(!qu.isEmpty()){
+                //     qu.poll();
+                // } 
+                vowelCount--;
             }
 
-            maxVowelCount = Math.max(maxVowelCount,qu.size());
+            maxVowelCount = Math.max(maxVowelCount,vowelCount);
 
         }
 
