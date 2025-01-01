e20ba6187d24cb5cccab20d00dcb49262af44674
Priyansh Nagar <50418088+priyanshn8387@users.noreply.github.com>
2025-01-02 00:31:31 +0530

    Time: 15 ms (92.37%), Space: 45 MB (32.21%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -12,7 +12,7 @@ class Solution {
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + s1.charAt(i - 1);
+                    dp[i][j] = dp[i - 1][j - 1] + s1.charAt(i - 1); // this line is imp...
                 } else {
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
@@ -25,7 +25,7 @@ class Solution {
         for (char c : s2.toCharArray()) totalASCIISum += c;
 
         // Subtract 2 * LCS ASCII sum (characters not deleted)
-        int lcsASCIISum = dp[m][n];
+        int lcsASCIISum = dp[m][n]; // store the ascii value present at last index
         return totalASCIISum - 2 * lcsASCIISum;
         
 
