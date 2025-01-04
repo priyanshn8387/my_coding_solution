dae5fa56dc51be069124218069174a2b361aced1
Priyansh Nagar <50418088+priyanshn8387@users.noreply.github.com>
2025-01-04 12:55:50 +0530

    Time: 0 ms (100%), Space: 40 MB (97.12%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,21 +1,19 @@
 class Solution {
 
-    public int recur(int i, int j, int[][] dp){
+    public int findPath(int i, int j, int[][] dp){
 
-        if(i<0 || j<0) return 0;
         if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
 
-        if(dp[i][j]!=-1) return dp[i][j];
+        if(dp[i][j]!= -1) return dp[i][j];
 
-
-        return dp[i][j]=recur(i-1,j,dp)+recur(i,j-1,dp);
+        return dp[i][j]=findPath(i-1,j,dp)+findPath(i,j-1,dp);
     }
     public int uniquePaths(int m, int n) {
-
-        int [][] dp = new int[m][n];
-        for(int[] row : dp) Arrays.fill(row,-1);
-
-        return recur(m-1,n-1,dp);
-        
+        int[][] dp = new int[m][n];
+         for (int[] row : dp) {
+            Arrays.fill(row, -1); // fill each row with -1
+        }
+        return findPath(m-1,n-1,dp);
     }
 }
