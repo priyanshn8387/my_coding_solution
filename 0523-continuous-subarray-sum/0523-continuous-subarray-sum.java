d1f1bec56ddd6da161431d9b35bbee50e1c1e245
Priyansh Nagar <50418088+priyanshn8387@users.noreply.github.com>
2025-01-05 16:12:50 +0530

    Time: 28 ms (26.14%), Space: 62.2 MB (21.16%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+
+        Map<Integer,Integer> prefixMap = new HashMap<>();
+        prefixMap.put(0,-1);
+
+        // replace sum with sum%k, coz it should be multiple of k
+
+        int sum=0;
+
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+
+            if(prefixMap.containsKey(sum%k)){ // sum-k ->sum%k-k%k->sum%k-0->sum%k
+                int subArrayStartIndex = prefixMap.get(sum%k)+1;
+                int subArrayEndIndex = i;
+                int len = subArrayEndIndex - subArrayStartIndex +1;
+
+                if(len>=2){
+                    return true;
+                }
+
+            }
+
+            if(!prefixMap.containsKey(sum%k))
+                prefixMap.put(sum%k,i);
+        }
+
+        return false;
+
+        
+    }
+}
