49d25ecffbdfb7d8e48cdfe9eb304c3d356c0e52
Priyansh Nagar <50418088+priyanshn8387@users.noreply.github.com>
2025-01-27 13:59:22 +0530

    Time: 10 ms (33.13%), Space: 48.1 MB (44.51%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -4,9 +4,30 @@ class Solution {
         Map<String,List<String>> ans = new HashMap<>();
 
         for(String word : strs){
-            char[] ch = word.toCharArray();
-            Arrays.sort(ch);
-            String sorted = new String(ch);
+
+            // char[] ch = word.toCharArray();
+            // Arrays.sort(ch);
+            // String sorted = new String(ch); // make it optimised
+
+            int[] charFreq = new int[26];
+            for(char ch : word.toCharArray()){
+                charFreq[ch-'a']++;
+            }
+
+            // retrieve from charFreq with adding identifaction of space with "#"
+            StringBuilder sb = new StringBuilder();
+            for(int i=0;i<26;i++){ 
+                if(charFreq[i]!=0){
+                    int frequencyElem =charFreq[i];
+                    while(frequencyElem>0){
+                        sb.append((char)97+i);
+                        frequencyElem--;
+                    }
+                    
+                }
+            }
+
+            String sorted = sb.toString();
 
             if(!ans.containsKey(sorted)){
                 ans.put(sorted,new ArrayList<>());
