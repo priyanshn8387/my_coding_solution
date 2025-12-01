28ab2489866a9997e941b7b4738d91872921cafe
Priyansh Nagar <50418088+priyanshn8387@users.noreply.github.com>
2025-12-01 23:14:12 +0530

    Time: 2 ms (94.91%), Space: 48.2 MB (42.47%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,6 +1,10 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
 
+
+        // Check edge cases 
+        if(numbers == null || numbers.length<2) return new int[]{-1,-1};
+
         int left =0;
         int right = numbers.length-1;
 
@@ -10,7 +14,8 @@ class Solution {
             else if(sum<target) left++;
             else right--;
         }
-
+        
+        // if no pair found
         return new int[]{-1,-1};  
     }
 }
