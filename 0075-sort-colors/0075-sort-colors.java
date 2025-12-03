d6c2dc3f39b161599c354828d1a36367e2a71d9d
Priyansh Nagar <50418088+priyanshn8387@users.noreply.github.com>
2025-12-03 23:43:24 +0530

    Time: 0 ms (100%), Space: 43.3 MB (35.6%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,33 +1,33 @@
 class Solution {
-
-    public void swap(int i, int j,int[] nums){
-        int temp = nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
-
-    }
     public void sortColors(int[] nums) {
+
+       // if(nums.length==null || nums.length<3) return -1;
 
         int low=0;
         int mid=0;
         int high=nums.length-1;
 
         while(mid<=high){
-
-            if(nums[mid]==0){
-                swap(low,mid,nums);
-                low++;
-                mid++;
+            if(nums[mid]==2){
+                swap(nums,mid,high);
+               // mid++; // kyuki yaha kuch bhi aa skta h 0/1/2, isliye check krna pdega mid ko.
+                high--;
 
             }else if(nums[mid]==1){
                 mid++;
             }else{
-                swap(high,mid,nums);
-                high--;
+                swap(nums,mid,low);
+                mid++; // mid pein hmesh 1 aayesga aur vhi humey chahiye, isliye skip kr diya travesding mein
+                low++; 
             }
+
         }
-
-
         
+    }
+
+    public void swap(int[] nums, int i, int j){
+        int temp= nums[j];
+        nums[j]=nums[i];
+        nums[i]=temp;
     }
 }
