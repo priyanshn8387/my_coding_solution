429efca5f91332c992dfc55c4afd531c281458a3
Priyansh Nagar <50418088+priyanshn8387@users.noreply.github.com>
2025-12-13 22:10:50 +0530

    Time: 2 ms (92.11%), Space: 64.7 MB (61.46%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,34 +1,36 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
 
-       // here it clearly says you cant use divide
-       // so need to think of another approach
-       // 1,2,3,4
-       //     i      for solving this i, you need (i-1 to 0)*(i+1 to N)
-       // so we maintin two array prefixMult and suffixMult
 
-       int n=nums.length; 
-       int[] prefixMult = new int[n];
-       int[] suffixMult = new int[n];
 
-       // now storing all multiplication in for i -> (i-1 to 0) 
-       prefixMult[0]=1; // at index 0, the previous value is 1;
-       for(int i=1;i<n;i++){
-            prefixMult[i]=prefixMult[i-1]*nums[i-1];
-       }
+        int n = nums.length;
 
-       suffixMult[n-1]=1; // at index 0, the previous value is 1;
-       for(int i=n-2;i>=0;i--){
-            suffixMult[i]=suffixMult[i+1]*nums[i+1];
-       }
+        int [] left = new int[n];
+        int [] right = new int[n];
 
-       // now calculate the anser
-       int[] ans = new int[n];
-       for(int i=0;i<n;i++){
-            ans[i]=prefixMult[i]*suffixMult[i];
-       }
+        left[0]=1;
+        right[n-1]=1;
+        int prodL=1;
+        int prodR=1;
 
-       return ans;
+        for(int i=1;i<n;i++){
+            prodL = prodL*nums[i-1];
+            left[i]=prodL;
+        }
+
+        for(int i=n-2;i>=0;i--){
+            prodR = prodR*nums[i+1];
+            right[i]=prodR;
+        }
+
+        int [] res = new int[n];
+        for(int i=n-1;i>=0;i--){
+            res[i]=left[i]*right[i];
+        }
+
+        return res;
         
     }
+
+    
 }
