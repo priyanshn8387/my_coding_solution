08437549462700b9bd24074eee5eeb491a075008
Priyansh Nagar <50418088+priyanshn8387@users.noreply.github.com>
2025-12-13 23:12:55 +0530

    Time: 1 ms (96.19%), Space: 46.8 MB (66.51%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -3,30 +3,23 @@ class Solution {
 
         int n = nums.length;
 
-        int [] left = new int[n];
-        int [] right = new int[n];
-
-        left[0]=0;
-        right[n-1]=0;
-        int prodL=0;
-        int prodR=0;
-
-        for(int i=1;i<n;i++){
-            prodL = prodL+nums[i-1];
-            left[i]=prodL;
-        }
-
-        for(int i=n-2;i>=0;i--){
-            prodR = prodR+nums[i+1];
-            right[i]=prodR;
+        int leftSum=0;
+        int rightSum=0;
+        for(int i=0;i<n;i++){
+            rightSum+=nums[i];
         }
 
         int res = -1;
-        for(int i=0;i<nums.length;i++){
-            if(left[i]==right[i]){
+
+        for(int i=0;i<n;i++){
+            rightSum-=nums[i];
+
+            if(leftSum==rightSum){
                 res = i;
                 break;
-            } 
+            }
+
+            leftSum+=nums[i]; 
         }
 
         return res;
