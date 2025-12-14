6c7c9abc331bdd5c31d94f7e83e59e06d36071c2
Priyansh Nagar <50418088+priyanshn8387@users.noreply.github.com>
2025-12-15 00:17:44 +0530

    Time: 47 ms (8.58%), Space: 47.4 MB (7.82%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+
+        if(s.length()<k) return 0;
+
+        Map<Character,Integer> freqMap = new HashMap<>();
+
+        int left=0;
+        int right =0;
+        int maxFreq = 0;
+        int len =0;
+        int maxLen = 0;
+
+
+        while(right<s.length()){
+            char incoming = s.charAt(right);
+            freqMap.put(incoming,freqMap.getOrDefault(incoming,0)+1);
+
+            maxFreq = Math.max(maxFreq,freqMap.get(incoming));
+            len = right-left+1;
+
+            if(len-maxFreq>k){
+                while(len-maxFreq>k){
+                    char outgoing = s.charAt(left);
+                    if(freqMap.get(outgoing)==1){
+                        freqMap.remove(outgoing);
+                    }else{
+                        freqMap.put(outgoing,freqMap.getOrDefault(outgoing,0)-1);
+                    }
+                    // Updating maxFreq on shrinkning
+                    maxFreq=0;
+                    for(Map.Entry<Character,Integer> it : freqMap.entrySet()){
+                        maxFreq=Math.max(maxFreq,it.getValue());
+                    }
+                    left++;
+                    len = right-left+1;
+                }
+            }
+
+            maxLen = Math.max(maxLen,right-left+1);
+
+            right++;
+        }
+
+        return maxLen;
+
+        
+    }
+}
