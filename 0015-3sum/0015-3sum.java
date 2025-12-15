e399cb58169887857ebfc156b1d21299ad53368b
Priyansh Nagar <50418088+priyanshn8387@users.noreply.github.com>
2025-12-15 16:47:30 +0530

    Time: 908 ms (7.65%), Space: 59.8 MB (15.21%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,34 +1,46 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        
-        Arrays.sort(nums);
-        int n= nums.length;
-        int target = 0;
-       
+    public List<List<Integer>> threeSum(int[] arr) {
+
         List<List<Integer>> res = new ArrayList<>();
-        Set<List<Integer>> s = new HashSet<>();
-        for (int i = 0; i < nums.length; i++){
-            int j = i + 1;
-            int k = nums.length - 1;
-           
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum == target) {
-                    s.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-                    k--;
-                } else if (sum < target) {
-                    j++;
-                } else {
-                    k--;
+        Set<List<Integer>> unique = new HashSet<>();
+
+        if(arr == null || arr.length <3) return res;
+
+        Arrays.sort(arr);
+        int len = arr.length;
+
+        for(int i=0;i<len-2;i++){
+
+            int num1 = arr[i];
+
+            // two pointer approach
+
+            int left =  i+1;
+            int right = len-1;
+
+            while(left<right){
+                int num2 = arr[left];
+                int num3 = arr[right];
+                int sum = num1+num2+num3;
+
+                if(sum==0){
+                    unique.add(Arrays.asList(num1,num2,num3));
+                    // finding more triplet
+                    left++;
+                    right--;
+                }else if(sum>0){
+                    right--;
+                }else {
+                    left++;
                 }
             }
-            
         }
 
-        res.addAll(s);
-
+        //return res.addAll(unique); 
+        res.addAll(unique);
         return res;
+
+
         
     }
 }
