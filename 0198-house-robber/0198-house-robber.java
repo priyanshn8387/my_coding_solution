e00dbc374ab359caf21fe60e4e67fc653698860d
Priyansh Nagar <50418088+priyanshn8387@users.noreply.github.com>
2025-12-16 15:35:32 +0530

    Time: 0 ms (100%), Space: 42.6 MB (52.87%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -16,26 +16,29 @@ class Solution {
     }
     public int rob(int[] nums) {
         int n = nums.length;
-        int [] dp = new int[n];
-        Arrays.fill(dp,-1);
+        // int [] dp = new int[n]; // not required n+1, coz f(4) can be stored in n size array
+        // Arrays.fill(dp,-1);
 
-        return maximumMoney(nums,dp,n-1);
+        // return maximumMoney(nums,dp,n-1);
 
 
         // bottom up 
 
-        //int [] dp = new int[n+1]; // change it to dp[n]
-        // int [] dp = new int[n]; 
-        // dp[0]=nums[0];
-        // dp[1]= Math.max(nums[0],nums[1]);
+         // base case for nums array
+         if (nums.length == 0) return 0; // Handle edge case where nums is empty
+          if (nums.length == 1) return nums[0]; // Handle edge case with a single house
 
-        // for(int i=2;i<n;i++){
-        //     int take = nums[i]+dp[i-2];
-        //     int notTake = 0+dp[i-1];
+        int [] dp = new int[n]; 
+        dp[0]=nums[0];
+        dp[1]= Math.max(nums[0],nums[1]);
 
-        //     dp[i]=Math.max(take,notTake);
-        // }
+        for(int i=2;i<n;i++){
+            int take = nums[i]+dp[i-2];
+            int notTake = 0+dp[i-1];
 
-        // return dp[n-1]; // since we are building from last then ans will be stored in top f(4);
+            dp[i]=Math.max(take,notTake);
+        }
+
+        return dp[n-1]; // since we are building from last then ans will be stored in top f(4);
     }
 }
