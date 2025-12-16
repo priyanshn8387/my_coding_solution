eff48aa087748db3ea771daa7eee38b08c9264fd
Priyansh Nagar <50418088+priyanshn8387@users.noreply.github.com>
2025-12-16 15:33:09 +0530

    Time: 0 ms (100%), Space: 42.8 MB (36.51%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -3,7 +3,7 @@ class Solution {
     public int maximumMoney(int[] nums, int[] dp , int index){
 
         if(index==0) return nums[index];
-        if(index==1) return Math.max(nums[index-1],nums[index]);
+        if(index==1) return Math.max(nums[index-1],nums[index]); // two houses will be there, yaa tou pehla ya dusra lootengey tou jo maxium hoga vhi lutengety.
 
         if(dp[index]!=-1) return dp[index];
  
@@ -16,9 +16,26 @@ class Solution {
     }
     public int rob(int[] nums) {
         int n = nums.length;
-        int [] dp = new int[n+1];
+        int [] dp = new int[n];
         Arrays.fill(dp,-1);
 
         return maximumMoney(nums,dp,n-1);
+
+
+        // bottom up 
+
+        //int [] dp = new int[n+1]; // change it to dp[n]
+        // int [] dp = new int[n]; 
+        // dp[0]=nums[0];
+        // dp[1]= Math.max(nums[0],nums[1]);
+
+        // for(int i=2;i<n;i++){
+        //     int take = nums[i]+dp[i-2];
+        //     int notTake = 0+dp[i-1];
+
+        //     dp[i]=Math.max(take,notTake);
+        // }
+
+        // return dp[n-1]; // since we are building from last then ans will be stored in top f(4);
     }
 }
