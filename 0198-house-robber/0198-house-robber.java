84448a7153981a6f058d1fe644532a29452a6fed
Priyansh Nagar <50418088+priyanshn8387@users.noreply.github.com>
2025-12-16 15:01:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,70 +1,25 @@
 class Solution {
-    // public int recur(int[] nums, int index, int [] dp){
-    //     //1. base case
-    //     if(index==0) return nums[0];
-    //     if(index<0) return 0;
 
-    //     //2. dp base case
+    public int maximumMoney(int[] nums, int[] dp , int index){
 
-    //     if(dp[index]!=-1) return dp[index];
+        if(index==0) return nums[index];
 
+        if(dp[index]!=-1) return dp[index];
 
-    //     // 3. recursion equation
+        int take = Integer.MIN_VALUE;
+        if(index>1){
+            take = nums[index]+maximumMoney(nums,dp,index-2);
+        }
+        int notTake = 0+maximumMoney(nums,dp,index-1);
 
-    //     int pick = nums[index]+recur(nums,index-2,dp);
-    //     int notPick = 0+ recur(nums,index-1,dp);
+        return dp[index]=Math.max(take,notTake);
 
-
-    //     // 4. store answer in dp
-
-    //     return dp[index]=Math.max(pick,notPick);
-    // }
+    }
     public int rob(int[] nums) {
-        int index = nums.length-1;
-        int[] dp = new int[nums.length];
+        int n = nums.length;
+        int [] dp = new int[n+1];
         Arrays.fill(dp,-1);
 
-        // 1. top down approach
-       // int ans=recur(nums,index,dp); 
-       // return ans;
-
-       // 2. bottom up 
-        // base case for nums array
-        // if (nums.length == 0) return 0; // Handle edge case where nums is empty
-        //  if (nums.length == 1) return nums[0]; // Handle edge case with a single house
-
-        // base case for dp array
-        // dp[0]=nums[0];
-
-        // for(int i=1;i<nums.length;i++){
-          
-        //     int pick = nums[i]+ ((i-2<0) ? 0 :dp[i-2]);
-            
-        //     int notPick = 0+ dp[i-1];
-        //     dp[i]=Math.max(pick,notPick);
-        // }
-
-        // return dp[index];
-
-        // 3. space optimised 
-        int prev2= nums[0];
-        int prev1 = 0;
-
-        for(int i=1;i<nums.length;i++){
-            int pick = nums[i]+ prev1;
-            int notPick = 0+ prev2;
-            int curr=Math.max(pick,notPick);
-
-            prev1=prev2;
-            prev2=curr;
-        }
-
-        return prev2;
-
-
-
-
-
-        
+        return maximumMoney(nums,dp,n-1);
     }
 }
