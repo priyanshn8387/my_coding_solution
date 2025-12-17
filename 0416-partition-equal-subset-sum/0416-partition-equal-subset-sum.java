01f176c5a4b54a1c575b3a5c4255c0b22f849538
Priyansh Nagar <50418088+priyanshn8387@users.noreply.github.com>
2025-12-17 22:56:46 +0530

    Time: 83 ms (30.77%), Space: 72.7 MB (25.23%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,140 +1,34 @@
 class Solution {
 
-    public boolean recur(int[] nums, int index, int target, int[][] dp) {
-        if (target == 0) return true;
+    public boolean isPartition(int[] nums, Boolean [][] dp, int target, int index){
 
-        if (index == 0) return (nums[index] == target ? true : false);
+        if(target==0) return true;
+        if(index==0) return nums[index]==target;
 
-        if (dp[index][target] != -1) return (dp[index][target] == 1 ? true : false);
-
+        if(dp[index][target]!=null) return dp[index][target];
         boolean pick = false;
-        if (target >= nums[index]) {
-            pick = recur(nums, index - 1, target - nums[index], dp);
+        if(target>=nums[index]){
+            pick = isPartition(nums,dp,target-nums[index],index-1);
         }
+        boolean notPick = isPartition(nums,dp,target,index-1);
 
-        boolean notPick = recur(nums, index - 1, target, dp);
-
-        dp[index][target] = (pick || notPick) ? 1 : 0;
-        return (pick || notPick);
+        return dp[index][target] = pick || notPick;
+        
     }
-
-    /* top-down approach
     public boolean canPartition(int[] nums) {
-        int totalSum = 0;
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            totalSum += nums[i];
-        }
-        // if number is odd, cant be partioned, so returning false
-        if (totalSum % 2 != 0) return false;
 
-        // if its even then proceed with half value
-
-        int index = nums.length - 1;
-        int[][] dp = new int[n][totalSum + 1];
-        for (int[] row : dp) Arrays.fill(row, -1);
-
-        return recur(nums, index, totalSum / 2, dp);
-    }
-    */
-
-    /* bottom up
-    public boolean canPartition(int[] nums) {
-        int totalSum = 0;
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            totalSum += nums[i];
-        }
-        // if number is odd, cant be partioned, so returning false
-        if (totalSum % 2 != 0) return false;
-
-        // if its even then proceed with half value
-        int target = totalSum / 2;
-        int index = nums.length - 1;
-        boolean[][] dp = new boolean[n][totalSum + 1]; // took boolean value
-
-        // converting base cases
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = true;
+        if(nums==null || nums.length<=1) return false;
+        int totalSum=0;
+        for(int i=0;i<nums.length;i++){
+            totalSum+=nums[i];
         }
 
-        for (int t = 0; t <= target; t++) {
-            if (nums[0] == t) dp[0][t] = true;
-        }
+        if(totalSum%2!=0) return false;
+        int target = totalSum/2;
 
-        // now iterate over loop i=0 and j=0 already solved
+        Boolean[][] dp = new Boolean[nums.length][target+1];
 
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j <= target; j++) {
-                boolean pick = false;
-                if (j >= nums[i]) {
-                    pick = dp[i-1][j - nums[i]];
-                    
-                }
-
-                boolean notPick = dp[i-1][j];
-
-                dp[i][j] = (pick || notPick);
-                
-            }
-        }
-
-        return dp[n-1][target];
-    }
-    */
-
-    public boolean canPartition(int[] nums) {
-        int totalSum = 0;
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            totalSum += nums[i];
-        }
-        // if number is odd, cant be partioned, so returning false
-        if (totalSum % 2 != 0) return false;
-
-        // if its even then proceed with half value
-        int target = totalSum / 2;
-        int index = nums.length - 1;
-
-        boolean[] prev = new boolean[target+1];
-       
-
-        // converting base cases, this means 0th column must be true,
-        // for (int i = 0; i < n; i++) {
-        //     dp[i][0] = true;
-        // }
-        // so converting for 1d array
-        prev[0]=true;
-      
-
-        for (int t = 0; t <= target; t++) {
-            if (nums[0] == t) prev[t] = true;
-        }
-
-        // now iterate over loop i=0 and j=0 already solved
-        // replacing dp[i-1] to prev and dp[i] to curr
-        // 
-
-        for (int i = 1; i < n; i++) {
-
-            boolean[] curr = new boolean[target+1];
-            curr[0]=true;
-            
-            for (int j = 1; j <= target; j++) {
-                boolean pick = false;
-                if (j >= nums[i]) {
-                    pick = prev[j - nums[i]];
-                }
-
-                boolean notPick = prev[j];
-
-                curr[j] = (pick || notPick);
-            }
-
-            // changing refernce 
-            prev = curr;
-        }
-
-        return prev[target];
+        return isPartition(nums,dp,target,nums.length-1);
+        
     }
 }
