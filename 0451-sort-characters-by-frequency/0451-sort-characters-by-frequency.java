183426e987ef3e3128de9f5a2a84a4354575a84e
Priyansh Nagar <50418088+priyanshn8387@users.noreply.github.com>
2026-01-11 00:28:01 +0530

    Time: 14 ms (60.13%), Space: 46.6 MB (55.03%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,36 +1,28 @@
 class Solution {
-
     public String frequencySort(String s) {
 
-        Map<Character, Integer> freqMap = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            freqMap.put(s.charAt(i), freqMap.getOrDefault(s.charAt(i), 0) + 1);
+        Map<Character,Integer> freq = new HashMap<>();
+
+        for(int i=0;i<s.length();i++){
+            freq.put(s.charAt(i),freq.getOrDefault(s.charAt(i),0)+1);
         }
 
-        // now create max heap with map with frequency in descensing order
-        PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+    List<Map.Entry<Character,Integer>> array= new ArrayList<>(freq.entrySet());
 
-        for (Map.Entry<Character, Integer> entry : freqMap.entrySet()) {
-            pq.offer(entry);
-        }
+        Collections.sort(array,(a,b)->b.getValue()-a.getValue());
 
         StringBuilder sb = new StringBuilder();
-        while(!pq.isEmpty()){
-           
-           // char key =pq.poll().getKey(); // this is wrong
-           // int val = pq.poll().getValue(); // this is wrong, you are calling poll two times
 
-            Map.Entry<Character,Integer> data = pq.poll();
-            char key =data.getKey(); 
-            int val = data.getValue();
-
+        for(int i=0;i<array.size();i++){
+            char key = array.get(i).getKey();
+            int val = array.get(i).getValue();
             while(val>0){
                 sb.append(key);
                 val--;
             }
-
         }
 
         return sb.toString();
+        
     }
 }
