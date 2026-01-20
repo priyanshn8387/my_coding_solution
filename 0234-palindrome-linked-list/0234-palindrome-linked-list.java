e4392def7fcc8d6ad8dcd0b74dc72578c7c61e33
Priyansh Nagar <50418088+priyanshn8387@users.noreply.github.com>
2026-01-20 23:39:05 +0530

    Time: 5 ms (52.92%), Space: 96.8 MB (32.69%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,57 +10,57 @@
  */
 class Solution {
 
-    public ListNode reverseLL(ListNode head){
-        if(head==null || head.next==null) return head;
+    public ListNode reverse(ListNode head){
 
+        if(head==null || head.next==null) return head;
         ListNode prev = null;
-        ListNode curr= head;
+        ListNode curr = head;
         ListNode frwd = curr.next;
 
         while(curr!=null){
             curr.next=prev;
             prev=curr;
             curr=frwd;
-            if(frwd!=null) frwd=frwd.next;
+            if(frwd!=null)frwd=frwd.next;
         }
 
         return prev;
     }
     public boolean isPalindrome(ListNode head) {
 
-
         if(head==null || head.next==null) return true;
+
         ListNode slow = head;
         ListNode fast = head;
 
-        while(fast.next!=null && fast.next.next!=null){
+        while(fast.next!=null && fast.next.next!=null){ // IMP
             slow=slow.next;
             fast=fast.next.next;
         }
 
-        ListNode interSectionPoint = slow;
+        ListNode mid = slow;
+        ListNode revHead = reverse(slow.next);
+        
+        ListNode start = head;
+        ListNode end = revHead;
+       
 
-        ListNode reverseHead = reverseLL(slow.next);
-
-        ListNode start= head;
-        ListNode reverseHeadList = reverseHead;
-        boolean found = true;
-
-        while(reverseHeadList!=null){
-            if(reverseHeadList.val!=start.val){
-                found=false;
-                break;
+        while(end!=null){
+            if(start.val!=end.val){
+                reverse(revHead);
+                return false;
             }
-            reverseHeadList=reverseHeadList.next;
-            start=start.next;
+            start= start.next;
+            end = end.next;
         }
 
-        // now joining back the list
+        // Now Joining back
+        reverse(revHead);
+       
 
-        reverseLL(reverseHead);
 
-        return found;
 
+        return true;
 
         
     }
