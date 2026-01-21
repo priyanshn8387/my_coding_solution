3963c2a16f44223576b92dc85cf68d002bee1b22
Priyansh Nagar <50418088+priyanshn8387@users.noreply.github.com>
2026-01-22 01:50:53 +0530

    Time: 0 ms (100%), Space: 46.6 MB (84.52%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,23 +1,18 @@
 class Solution {
 
-    public void dfs(int i, int j, int[][] image, boolean [][] visited, int initialColor, int targetColor){
+    public void dfs(int i, int j, int[][] image,int targetColor, int initialColor){
 
-    if(i<0 || i>=image.length || j<0 || j>=image[0].length || image[i][j]!=initialColor || visited[i][j]){
+    if(i<0 || i>=image.length || j<0 || j>=image[0].length || image[i][j]!=initialColor || image[i][j]==targetColor){
             return;
         }
 
         // fillthe color
-        visited[i][j]=true;
         image[i][j]=targetColor;
 
-        int[][] dirs = {{i-1,j},{i,j+1},{i+1,j},{i,j-1}};
-        for(int[] dir : dirs){
-            int i_ = dir[0];
-            int j_ = dir[1];
-
-            dfs(i_,j_,image,visited,initialColor,targetColor);
-
-        }
+        dfs(i,j+1,image,targetColor,initialColor);
+        dfs(i-1,j,image,targetColor,initialColor);
+        dfs(i,j-1,image,targetColor,initialColor);
+        dfs(i+1,j,image,targetColor,initialColor);
 
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
@@ -25,12 +20,12 @@ class Solution {
         int row = image.length;
         int col = image[0].length;
 
-        boolean[][] visited = new boolean[row][col];
+        int initialColor = image[sr][sc];
 
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
-                if(i==sr && j==sc && !visited[i][j]){
-                    dfs(i,j,image,visited,image[sr][sc],color);
+                if(i==sr && j==sc){
+                    dfs(i,j,image,color,initialColor);
                 }
             }
         }
