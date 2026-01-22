cbffed06cec31ce8204ab9d5e146316715e87bb1
Priyansh Nagar <50418088+priyanshn8387@users.noreply.github.com>
2026-01-22 20:26:59 +0530

    Time: 6 ms (31.39%), Space: 47.6 MB (14.91%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,27 +1,39 @@
 class Solution {
+
+    public int dfs(int i, int j, int[][] grid){
+
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==0){
+            return 1;
+        }
+
+        if(grid[i][j]==-1) return 0;
+
+        grid[i][j]=-1;
+
+        int right = dfs(i,j+1,grid);
+        int bottom = dfs(i+1,j,grid);
+        int left = dfs(i,j-1,grid);
+        int top = dfs(i+1,j,grid);
+
+        return right+bottom+left+top;
+
+
+    }
     public int islandPerimeter(int[][] grid) {
-        
+
         int row = grid.length;
         int col = grid[0].length;
-        int count =0;
-
+        int total =0;
 
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 if(grid[i][j]==1){
-
-                    // top (checking outofbound and water condition)
-                    if(i>0 && grid[i-1][j]==0 || i==0) count++;
-                    // right
-                    if(j<col-1 && grid[i][j+1]==0 || j==col-1) count++;
-                    // left
-                    if(i<row-1 && grid[i+1][j]==0 || i==row-1) count++;
-                    // bottom
-                    if(j>0 && grid[i][j-1]==0 || j==0) count++;
+                    total+=dfs(i,j,grid);
                 }
             }
         }
 
-        return count;
+        return total;
+        
     }
 }
